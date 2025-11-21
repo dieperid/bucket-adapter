9e2392eab3a49e706823348f8a4775bec611cc91
dieperid <david.dieperink@ik.me>
2025-11-21 16:25:29 +0100

    feat: implemented list mock

--- src/main/java/com/example/bucketadapter/providers/AWSSDK.java
+++ src/main/java/com/example/bucketadapter/providers/AWSSDK.java
@@ -8,5 +8,8 @@ public class AWSSDK {
     public void putObject(String src, String dest) {}
     public void getObject(String src, String dest) {}
     public void deleteObject(String src) {}
-    public List<String> listObjects(String src) { return List.of(); }
+
+    public List<String> listObjects(String src) {
+        return List.of("file1.txt", "file2.txt", "folder/file3.txt");
+    }
 }
