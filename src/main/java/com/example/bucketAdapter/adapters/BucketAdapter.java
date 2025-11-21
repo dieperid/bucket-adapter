197085b9610f4355b7cb84e54e2f4c8d85814e94
dieperid <david.dieperink@ik.me>
2025-11-21 14:51:55 +0100

    feat: added BucketAdapter interface

---
+++ src/main/java/com/example/bucketAdapter/adapters/BucketAdapter.java
@@ -0,0 +1,10 @@
+package com.example.bucketAdapter.adapters;
+
+import java.util.List;
+
+public interface BucketAdapter {
+    void upload(String localSrc, String remoteSrc);
+    void download(String localSrc, String remoteSrc);
+    void delete(String remoteSrc, boolean recursive);
+    List<String> list(String remoteSrc);
+}
