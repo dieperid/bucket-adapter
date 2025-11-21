63bc640f8173e879ac1951a634f93dade351ab10
dieperid <david.dieperink@ik.me>
2025-11-21 14:27:15 +0100

    chore: initial commit

---
+++ src/main/java/com/example/bucketAdapter/BucketAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.bucketAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bucketAdapter/BucketAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bucketAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
