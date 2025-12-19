00702494c5ceb7544081276ed77a4224438263a9
dieperid <david.dieperink@ik.me>
2025-12-19 17:26:32 +0100

    refactor: removed dependancies injection of ApplicationContext and fetch the provider from .env

--- src/main/java/com/example/bucketadapter/factory/BucketAdapterFactory.java
+++ src/main/java/com/example/bucketadapter/factory/BucketAdapterFactory.java
@@ -1,28 +1,23 @@
 package com.example.bucketadapter.factory;
 
-import org.springframework.context.ApplicationContext;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import com.example.bucketadapter.adapter.BucketAdapter;
-import com.example.bucketadapter.adapter.impl.AwsAdapterImpl;
-import com.example.bucketadapter.adapter.impl.AzureAdapterImpl;
-import com.example.bucketadapter.adapter.impl.GcpAdapterImpl;
-import com.example.bucketadapter.model.CloudProvider;
 
 @Component
 public class BucketAdapterFactory {
 
-    private final ApplicationContext context;
+    @Autowired
+    private Map<String, BucketAdapter> adapters;
 
-    public BucketAdapterFactory(ApplicationContext context) {
-        this.context = context;
-    }
+    public BucketAdapter getAdapter() {
+        String provider = System.getenv("PROVIDER_IMPL");
 
-    public BucketAdapter getAdapter(CloudProvider provider) {
-        return switch (provider) {
-            case AWS -> context.getBean(AwsAdapterImpl.class);
-            case AZURE -> context.getBean(AzureAdapterImpl.class);
-            case GCP -> context.getBean(GcpAdapterImpl.class);
-        };
+        BucketAdapter adapter = adapters.get(provider);
+        if (adapter == null) {
+            throw new IllegalArgumentException("Unsupported provider: " + provider);
+        }
+        return adapter;
     }
 }
