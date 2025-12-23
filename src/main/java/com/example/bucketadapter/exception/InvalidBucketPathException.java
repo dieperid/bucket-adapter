15d9097e95e88be52c35995c9975a7687a43156a
dieperid <david.dieperink@ik.me>
2025-12-23 10:54:37 +0100

    feat: added Exception class

---
+++ src/main/java/com/example/bucketadapter/exception/InvalidBucketPathException.java
@@ -0,0 +1,7 @@
+package com.example.bucketadapter.exception;
+
+public class InvalidBucketPathException extends RuntimeException {
+    public InvalidBucketPathException(String message) {
+        super(message);
+    }
+}
