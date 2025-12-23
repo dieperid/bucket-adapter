00c5d333193f02eb89a65ce4594fd904f067eb94
dieperid <david.dieperink@ik.me>
2025-12-23 18:53:51 +0100

    refactor: start the app with the DotenvInitializer

--- src/main/java/com/example/bucketadapter/BucketAdapterApplication.java
+++ src/main/java/com/example/bucketadapter/BucketAdapterApplication.java
@@ -1,13 +1,17 @@
 package com.example.bucketadapter;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.example.bucketadapter.config.DotenvInitializer;
 
 @SpringBootApplication
 public class BucketAdapterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BucketAdapterApplication.class, args);
+        new SpringApplicationBuilder(BucketAdapterApplication.class)
+                .initializers(new DotenvInitializer())
+                .run(args);
     }
 
 }
